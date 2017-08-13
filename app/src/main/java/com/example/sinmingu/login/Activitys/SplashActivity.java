d088e1f8eb692b709adc8d4ea2c2cf70e7e5dd37
ImHyunChang <dlafkaqh@kpu.ac.kr>
2017-08-13 15:44:40 +0900

    make pakages

--- app/src/main/java/com/example/sinmingu/login/Activitys/SplashActivity.java
+++ app/src/main/java/com/example/sinmingu/login/Activitys/SplashActivity.java
@@ -1,9 +1,11 @@
-package com.example.sinmingu.login;
+package com.example.sinmingu.login.Activitys;
 
 import android.app.Activity;
 import android.os.Bundle;
 import android.os.Handler;
 import android.support.annotation.Nullable;
+
+import com.example.sinmingu.login.R;
 
 /**
  * Created by sinmingu on 2017-08-11.
